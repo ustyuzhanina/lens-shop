3b8cd3cfc31ce93ef0f72c4a3db49bbefdad3521
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-06-01 21:51:20 +0700

    logo text added

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import "./styles/style.scss";
 // import { Tooltip, Toast, Popover } from "bootstrap";
 
 import "./js/common";
+
+const NAVBAR = document.querySelector(".navbar");
+
+NAVBAR.addEventListener("click", (e) => {
+    e.target.classList.toggle("navbar_opened");
+});
